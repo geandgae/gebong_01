2c9a863f75c2f3db2e8e32567526d009b92450ae
김대화 <76902529@kyobo.com>
2022-12-08 15:55:14 +0900

    - layer 수정

--- app/src/assets/js/layer.js
+++ app/src/assets/js/layer.js
@@ -8,23 +8,41 @@
     let outer = document.querySelector(".outland");
     let btn = document.querySelectorAll(".btn[data-layer]");
     let close = document.querySelectorAll(".outland .close");
+    let focus;
+
+    const evtKeytab = function() {
+      let btn = document.querySelectorAll(".layer.focus .btn")
+      focus?.addEventListener("keyup", function(e) {
+        e.preventDefault();
+        if (window.event.keyCode == 9) {
+          btn.forEach(function(item) {
+            item.focus();
+            console.log(window.event.keyCode);
+          });
+        }
+      });
+    }
+
+    
 
     const evtOpen = function(e) {
       let id = e.currentTarget.dataset.layer;
       let target = document.querySelectorAll(".outland .layer");
       target.forEach(function(item) {
         if(id === item.dataset.layer) {
-          console.log(item);
           outer?.classList.add("active");
           item.classList.add("active");
-          item.classList.add("current");
+          item.classList.add("focus");
           item.setAttribute("tabindex", 0);
           item.setAttribute("aria-hidden", "false");
+          focus = item;
           item.focus();
+          console.log(focus);
+          evtKeytab();
         } else {
           item.setAttribute("tabindex", -1);
           item.setAttribute("aria-hidden", "true");
-          item.classList.remove("current");
+          item.classList.remove("focus");
         }
       });
     }
@@ -68,3 +86,69 @@
 })();
 // e : function
 
+
+const test = function(e) {
+
+  var focusedElementBeforeModal;
+  var modal = document.querySelector('.modal');
+  var modalOverlay = document.querySelector('.modal-overlay');
+  var modalToggle = document.querySelector('.modal-toggle');
+
+  modalToggle.addEventListener('click', openModal);
+
+  function openModal() {
+    focusedElementBeforeModal = document.activeElement;// focus된게 아니라 'active'된 el.
+
+    modal.addEventListener('keydown', trapTabKey);// key를 누르고 있을때
+    modalOverlay.addEventListener('click', closeModal);
+
+    var signUpBtn = modal.querySelector('#signup');
+    signUpBtn.addEventListener('click', closeModal);
+
+    var focusableElementsString = 'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable]'; //포커스가 갈 수 있는 엘레먼트
+    var focusableElements = modal.querySelectorAll(focusableElementsString); //querySelectorAll은 nodelist로 반환함
+    focusableElements = Array.prototype.slice.call(focusableElements); //노드 목록을 어레이로 변환, 변환하지 않아도 firstTabStop, lastTabStop 값 가져올 수 있음
+
+    var firstTabStop = focusableElements[0];
+    var lastTabStop = focusableElements[focusableElements.length - 1];
+
+    modal.style.display = 'block';
+    modalOverlay.style.display = 'block';
+
+    firstTabStop.focus();
+
+    function trapTabKey(e) {
+      // Check for TAB key press
+      if (e.keyCode === 9) {
+
+        // SHIFT + TAB
+        if (e.shiftKey) {
+          if (document.activeElement === firstTabStop) {
+          e.preventDefault();
+          lastTabStop.focus();
+          }
+
+        // TAB
+        } else {
+          if (document.activeElement === lastTabStop) {
+          e.preventDefault();
+          firstTabStop.focus();
+          }
+        }
+      }
+
+      // ESCAPE
+      if (e.keyCode === 27) {
+        closeModal();
+      }
+    }
+  }
+
+  function closeModal() {
+    modal.style.display = 'none';
+    modalOverlay.style.display = 'none';
+
+    focusedElementBeforeModal.focus();
+  }
+
+}

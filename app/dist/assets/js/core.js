3f73f757512548aa44a7f22b93eb80b55f905329
김대화 <76902529@kyobo.com>
2023-01-06 11:11:54 +0900

    - 스크롤 테스트

--- app/dist/assets/js/core.js
+++ app/dist/assets/js/core.js
@@ -2,6 +2,30 @@
 
 // s : function
 (function () {
-  console.log("core start !!!!!")
+  
+  console.log(document.readyState);
+  setTimeout(() => {
+    console.log(document.readyState);
+  }, 500);
+
+  
+
 })();
 // e : function
+
+docReady(function() {
+  startLog();
+});
+
+
+function docReady(fn) {
+	if (document.readyState === "complete" || document.readyState === "interactive") {
+		setTimeout(fn, 1);
+	} else {
+		document.addEventListener("DOMContentLoaded", fn);
+	}
+}
+
+function startLog() {
+  console.log("core start !!!!!");
+}

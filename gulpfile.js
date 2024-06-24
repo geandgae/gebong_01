91c975aac0aeaefbba54194df1413d72e8a72d3b
g_e_out <g_e@daum.net>
2024-06-25 08:15:19 +0900

    gulp-rename

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,7 @@ const sass = require("gulp-dart-sass");
 const cleanCSS = require("gulp-clean-css");
 const fs = require("fs-extra");
 const browserSync = require("browser-sync").create();
+// const rename = require('gulp-rename');
 
 
 // path
@@ -90,6 +91,19 @@ gulp.task("sass", function() {
     .pipe(gulp.dest(pathDist.css))
     .pipe(browserSync.stream());
 });
+
+// // SCSS 파일에 대해 분리된 CSS 파일로 컴파일하는 Gulp 작업 설정
+// gulp.task('sass-individual', function() {
+//   // SCSS 파일을 읽어와서 각각의 파일 별로 컴파일
+//   return gulp.src(path.join(pathSrc.scss, '*.scss'))
+//     .pipe(sass().on('error', sass.logError)) // SCSS를 CSS로 컴파일하고 에러 처리
+//     .pipe(rename(function(path) {
+//       // 각 SCSS 파일의 이름을 기반으로 CSS 파일 이름 설정
+//       path.basename = path.basename; // 파일의 기본 이름 유지
+//     }))
+//     .pipe(gulp.dest(pathSrc.css)) // 컴파일된 CSS 파일을 지정된 디렉터리에 저장
+//     .pipe(browserSync.stream()); // 브라우저에 변경 사항 실시간 반영
+// });
 
 // 폰트 파일 복사
 gulp.task("fonts", function() {
